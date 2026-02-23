424b13edb8d9c50ebe712eb1ca9ca472abd1a252
prashanth766 <chirlavanchaprashanth@gmail.com>
2026-02-23 16:00:58 +0530

    git-actions

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name ="test_vpc"
+    }
+  
+}
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      Name ="tf_subnet11"
+    }
+  
+}
+resource "aws_subnet" "name1" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      Name ="tf_subnet22"
+    }
+  
+}
